d6e4911d49abb3aebabfb8d74cecec02010becdc
Toth-Gabor <upcgabor@gmail.com>
2019-05-06 11:20:41 +0200

    user update

--- src/main/java/com/codecool/web/model/User.java
+++ src/main/java/com/codecool/web/model/User.java
@@ -4,15 +4,26 @@ import java.util.Objects;
 
 public final class User extends AbstractModel {
 
+    private String name;
+    private boolean admin;
     private final String email;
     private final String password;
-
-    public User(int id, String email, String password) {
+    
+    public User(int id, String name, boolean admin, String email, String password) {
         super(id);
+        this.name = name;
+        this.admin = admin;
         this.email = email;
         this.password = password;
     }
-
+    
+    public String getName() {
+        return name;
+    }
+    
+    public boolean isAdmin() {
+        return admin;
+    }
     public String getEmail() {
         return email;
     }
