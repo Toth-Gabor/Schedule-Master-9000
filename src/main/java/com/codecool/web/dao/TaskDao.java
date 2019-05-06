cd8de30749c72ab065499a0cb40f57156269bcbf
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-06 13:05:41 +0200

    taskdao interface created

---
+++ src/main/java/com/codecool/web/dao/TaskDao.java
@@ -0,0 +1,13 @@
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Task;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskDao {
+
+    List<Task> findbyName(String name) throws SQLException;
+    List<Task> findbyContent(String content) throws SQLException;
+    List<Task> findbyScheduleId(int scheduleId) throws SQLException;
+}
