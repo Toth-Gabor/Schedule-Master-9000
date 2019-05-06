48c6db383c251536bec9497191b6795e0c89fd80
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-06 11:23:20 +0200

    schedule model done

---
+++ src/main/java/com/codecool/web/model/Schedule.java
@@ -0,0 +1,20 @@
+package com.codecool.web.model;
+
+public class Schedule extends AbstractModel {
+    private boolean isPublished;
+    private int userId;
+
+    public Schedule(int id, boolean isPublished, int userId) {
+        super(id);
+        this.isPublished = isPublished;
+        this.userId = userId;
+    }
+
+    public boolean isPublished() {
+        return isPublished;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
