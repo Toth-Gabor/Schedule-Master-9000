bb2d8999f22b7c522e844a28baee10e4df90ee33
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-07 11:56:08 +0200

    implemented part of service

--- src/main/java/com/codecool/web/service/HourService.java
+++ src/main/java/com/codecool/web/service/HourService.java
@@ -1,16 +1,18 @@
 package com.codecool.web.service;
 
 import com.codecool.web.model.Hour;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface HourService {
 
-    List<Hour> findbyHourValue(int hourValue) throws SQLException;
-    List<Hour> findbyTaskId(int taskId) throws SQLException;
-    List<Hour> findbyDayId(int taskId) throws SQLException;
+    List<Hour> getAll() throws SQLException;
+    List<Hour> getbyHourValue(String hourValue) throws SQLException, ServiceException;
+    List<Hour> getbyTaskId(String taskId) throws SQLException, ServiceException;
+    List<Hour> getbyDayId(String dayId) throws SQLException, ServiceException;
     void delete(Hour hour) throws SQLException;
-    void add(int hourValue, int taskId, int dayId) throws SQLException;
-    void update(Hour hour, int hourValue) throws SQLException;
+    void add(String hourValue, String taskId, String dayId) throws SQLException, ServiceException;
+    void update(Hour hour, String hourValue) throws SQLException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleHourService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleHourService.java
@@ -1,25 +1,57 @@
 package com.codecool.web.service.simple;
 
+import com.codecool.web.dao.HourDao;
 import com.codecool.web.model.Hour;
 import com.codecool.web.service.HourService;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class SimpleHourService implements HourService {
-    @Override
-    public List<Hour> findbyHourValue(int hourValue) throws SQLException {
-        return null;
+    private final HourDao hourDao;
+
+
+    public SimpleHourService(HourDao hourDao) {
+        this.hourDao = hourDao;
     }
 
     @Override
-    public List<Hour> findbyTaskId(int taskId) throws SQLException {
-        return null;
+    public List<Hour> getAll() throws SQLException {
+       return hourDao.findAll();
     }
 
     @Override
-    public List<Hour> findbyDayId(int taskId) throws SQLException {
-        return null;
+    public List<Hour> getbyHourValue(String hourValue) throws SQLException, ServiceException {
+        try {
+            return hourDao.findbyHourValue(Integer.parseInt(hourValue));
+        }catch (NumberFormatException ex) {
+            throw new ServiceException("Hour value must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Hour> getbyTaskId(String taskId) throws SQLException, ServiceException {
+        try {
+            return hourDao.findbyTaskId(Integer.parseInt(taskId));
+        }catch (NumberFormatException ex) {
+            throw new ServiceException("Task ID must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Hour> getbyDayId(String dayId) throws SQLException, ServiceException {
+        try {
+            return hourDao.findbyDayId(Integer.parseInt(dayId));
+        }catch (NumberFormatException ex) {
+            throw new ServiceException("Day ID must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
     }
 
     @Override
@@ -28,12 +60,20 @@ public class SimpleHourService implements HourService {
     }
 
     @Override
-    public void add(int hourValue, int taskId, int dayId) throws SQLException {
+    public void add(String hourValue, String taskId, String dayId) throws SQLException, ServiceException {
+        try {
+            hourDao.add(Integer.parseInt(hourValue), Integer.parseInt(taskId), Integer.parseInt(dayId));
+        }catch (NumberFormatException ex) {
+            throw new ServiceException("Hour value must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
 
     }
 
     @Override
-    public void update(Hour hour, int hourValue) throws SQLException {
+    public void update(Hour hour, String hourValue) throws SQLException {
 
     }
+
 }
