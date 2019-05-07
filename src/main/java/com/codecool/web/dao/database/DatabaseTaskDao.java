6369f080b6bee1f12554c7a8a9e230c62f6744d2
Toth-Gabor <upcgabor@gmail.com>
2019-05-07 10:51:21 +0200

    taskdao add update method

--- src/main/java/com/codecool/web/dao/database/DatabaseTaskDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseTaskDao.java
@@ -92,6 +92,16 @@ public class DatabaseTaskDao extends AbstractDao implements TaskDao {
         }
     }
     
+    @Override
+    public void update(Task task, String name, String content) throws SQLException {
+        String sql = "UPDATE task SET task_name = ?, task_content = ? WHERE task_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, name);
+            statement.setString(2, content);
+            statement.setInt(3, task.getId());
+        }
+    }
+    
     private Task fetchTask(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("task_id");
         String name = resultSet.getString("task_name");
