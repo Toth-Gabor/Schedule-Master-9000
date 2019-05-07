a73b18d964f55b8c55a8ff7ef28b6d3dee951c70
Toth-Gabor <upcgabor@gmail.com>
2019-05-07 13:58:03 +0200

    task services implemented

---
+++ src/main/java/com/codecool/web/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Task;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskService {
+    
+    List<Task> getAll() throws SQLException;
+    List<Task> getbyName(String name) throws SQLException;
+    List<Task> getbyContent(String content) throws SQLException;
+    List<Task> getbyScheduleId(String scheduleId) throws SQLException, ServiceException;
+    void delete(Object o) throws SQLException, ServiceException;
+    void add(String name, String content, String scheduleId) throws SQLException, ServiceException;
+    void update(Object o, String name, String content) throws SQLException, ServiceException;
+}
---
+++ src/main/java/com/codecool/web/service/simple/SimpleTaskService.java
@@ -0,0 +1,73 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.model.Task;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class SimpleTaskService implements TaskService {
+    
+    private final TaskDao taskDao;
+    
+    public SimpleTaskService(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+    
+    @Override
+    public List<Task> getAll() throws SQLException {
+        return taskDao.findAll();
+    }
+    
+    @Override
+    public List<Task> getbyName(String name) throws SQLException {
+        return taskDao.findbyName(name);
+    }
+    
+    @Override
+    public List<Task> getbyContent(String content) throws SQLException {
+        return taskDao.findbyContent(content);
+    }
+    
+    @Override
+    public List<Task> getbyScheduleId(String scheduleId) throws SQLException, ServiceException {
+        try {
+            return taskDao.findbyScheduleId(Integer.parseInt(scheduleId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("Schedule id must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+    
+    @Override
+    public void delete(Object o) throws SQLException, ServiceException {
+        if (o instanceof Task){
+            taskDao.delete((Task) o);
+        } else {
+            throw new ServiceException("Not type of Task");
+        }
+    }
+    
+    @Override
+    public void add(String name, String content, String scheduleId) throws SQLException, ServiceException {
+        try {
+            taskDao.add(name, content, Integer.parseInt(scheduleId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("Schedule id must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+    
+    @Override
+    public void update(Object o, String name, String content) throws SQLException, ServiceException {
+        if (o instanceof Task){
+            taskDao.update((Task) o, name, content);
+        } else {
+            throw new ServiceException("Not type of Task");
+        }
+    }
+}
