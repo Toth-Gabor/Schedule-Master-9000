2d2696370315711557404e8d599db3602646056e
András Urbin <urbinandras@gmail.com>
2019-05-08 11:18:58 +0200

    -

--- src/main/webapp/profile.js
+++ src/main/webapp/profile.js
@@ -1,21 +1,4 @@
-function onShopsClicked() {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', onShopsResponse);
-    xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', 'protected/shops');
-    xhr.send();
-}
-
-function onCouponsClicked() {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', onCouponsResponse);
-    xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', 'protected/coupons');
-    xhr.send();
-}
-
 function onProfileLoad(user) {
     clearMessages();
     showContents(['add-schedule-content', 'edit-schedule-content', 'remove-schedule-content', 'header-content', 'form-buttons-content', 'schedule-table-content', 'profile-content', 'logout-content']);
-
 }
