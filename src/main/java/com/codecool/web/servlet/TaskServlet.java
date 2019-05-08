416d14f9c59056139d5310dd5def58d509393b66
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-08 11:15:40 +0200

    implemented get post put delete do methods

--- src/main/java/com/codecool/web/servlet/TaskServlet.java
+++ src/main/java/com/codecool/web/servlet/TaskServlet.java
@@ -1,31 +1,87 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Task;
+import com.codecool.web.model.User;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleTaskService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/protected/task")
 public class TaskServlet extends AbstractServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())){
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            int taskId = (Integer) req.getAttribute("task-id");
+            Task task = taskService.getbyId(taskId);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, task);
+
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        }
     }
-    
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())){
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            String taskName = (String) req.getAttribute("task-name");
+            String taskContent = (String) req.getAttribute("task-content");
+            taskService.add(taskName, taskContent);
+
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            String taskName = (String) req.getAttribute("task-name");
+            String taskContent = (String) req.getAttribute("task-content");
+            int taskId = (Integer) req.getAttribute("task-id");
+
+            Task task = taskService.getbyId(taskId);
+            taskService.update(task, taskName, taskContent);
+
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())){
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            int taskId = (Integer) req.getAttribute("task-id");
+            Task task = taskService.getbyId(taskId);
+            taskService.delete(task);
+
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
 }
