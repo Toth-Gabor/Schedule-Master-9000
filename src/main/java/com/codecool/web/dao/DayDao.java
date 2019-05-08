6c629740a51d63b78d06f145a860c11204efc963
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-08 14:46:37 +0200

    when adding a day now it requires a schedule object instead of a schedule id for more info

--- src/main/java/com/codecool/web/dao/DayDao.java
+++ src/main/java/com/codecool/web/dao/DayDao.java
@@ -1,6 +1,7 @@
 package com.codecool.web.dao;
 
 import com.codecool.web.model.Day;
+import com.codecool.web.model.Schedule;
 import org.springframework.jdbc.support.xml.SqlXmlFeatureNotImplementedException;
 
 import java.sql.SQLException;
@@ -13,6 +14,6 @@ public interface DayDao {
     List<Day> findByName(String name) throws SQLException;
     Day findByScheduleId(int scheduleId) throws SQLException;
     void delete(Day day) throws SQLException;
-    void add(String name, int scheduleId) throws SQLException;
+    void add(String name, Schedule schedule) throws SQLException;
     void update(Day day, String name) throws SQLException;
 }
