376de1239b6386e6137898aedbe0f9f743115329
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-08 14:49:47 +0200

    implemented find scheduleid by userid method, reformatted add method so it creates days as many as the intervallum with names

--- src/main/java/com/codecool/web/dao/ScheduleDao.java
+++ src/main/java/com/codecool/web/dao/ScheduleDao.java
@@ -7,14 +7,25 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface ScheduleDao {
-    
+
     List<Schedule> findAll() throws SQLException;
+
     List<Schedule> findbyIsPublished(boolean isPublished) throws SQLException;
+
     List<Schedule> findbyUserId(int userId) throws SQLException;
+
     Schedule findById(int scheduleId) throws SQLException;
+
     void delete(Schedule schedule) throws SQLException;
-    void add(boolean isPublished, int userId, int dayValue) throws SQLException;
-    void addTask(Task task, int scheduleId) throws SQLException;
-    void generateBody(int dayValue) throws SQLException;
+
+    void add(boolean isPublished, int userId, int dayValue, List<String> dayNames) throws SQLException;
+
+    void addTask(Task task, int scheduleId, int dayId, int hourId) throws SQLException;
+
+    int findScheduleId(int userId) throws SQLException;
+
     void update(Schedule schedule, boolean isPublished) throws SQLException;
+
+    void addDays(String dayName, int scheduleId) throws SQLException;
+
 }
