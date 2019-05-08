318369540278ae642fb226e20bc95a4c0c4827eb
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-08 11:39:33 +0200

    created tasks servlet with all methods

--- src/main/java/com/codecool/web/servlet/TasksServlet.java
+++ src/main/java/com/codecool/web/servlet/TasksServlet.java
@@ -1,4 +1,38 @@
 package com.codecool.web.servlet;
 
-public class TasksServlet {
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.model.Task;
+import com.codecool.web.model.User;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleTaskService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/protected/tasks")
+public class TasksServlet extends AbstractServlet {
+    private static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())){
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            User user = (User) req.getSession().getAttribute("user");
+            List<Task> tasks = taskService.getbyUserId(user.getId());
+
+            sendMessage(resp, HttpServletResponse.SC_OK, tasks);
+
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        }
+    }
 }
