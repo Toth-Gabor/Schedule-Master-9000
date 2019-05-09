cc69965ccbf5e35d15efb872560c8408dff90e7b
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-09 13:02:05 +0200

    removed taskid fields

--- src/main/java/com/codecool/web/model/Hour.java
+++ src/main/java/com/codecool/web/model/Hour.java
@@ -3,22 +3,16 @@ package com.codecool.web.model;
 public class Hour extends AbstractModel {
     
     private int hourValue;
-    private int taskId;
     private int dayId;
     
-    public Hour(int id, int hourValue, int taskId, int dayId) {
+    public Hour(int id, int hourValue, int dayId) {
         super(id);
         this.hourValue = hourValue;
-        this.taskId = taskId;
         this.dayId = dayId;
     }
     
     public int getHourValue() {
         return hourValue;
-    }
-    
-    public int getTaskId() {
-        return taskId;
     }
     
     public int getDayId() {
