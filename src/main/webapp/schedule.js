d2c21f2e99db93883b78268ee1a60c95575e1711
Toth-Gabor <upcgabor@gmail.com>
2019-05-09 15:46:45 +0200

    refactor schedule.js change var to let

--- src/main/webapp/schedule.js
+++ src/main/webapp/schedule.js
@@ -18,18 +18,18 @@ function onScheduleResponse() {
 
 function populateTable(table, time, rows, cells, content) {
     if (!table) table = document.createElement('table');
-    var head = document.createElement('thead');
+    let head = document.createElement('thead');
     table.appendChild(head);
 
-    for (var x = 1; x <= time; x++) {
-        var title = document.createElement('th');
+    for (let x = 1; x <= time; x++) {
+        let title = document.createElement('th');
         title.appendChild(document.createTextNode(x + '. Day'));
         head.appendChild(title);
     }
-    var body = document.createElement('tbody');
-    for (var i = 0; i < rows; ++i) {
-        var row = document.createElement('tr');
-        for (var j = 0; j < cells; ++j) {
+    let body = document.createElement('tbody');
+    for (let i = 0; i < rows; ++i) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < cells; ++j) {
             row.appendChild(document.createElement('td'));
             row.cells[j].appendChild(document.createTextNode(content + (j + 1)));
         }
