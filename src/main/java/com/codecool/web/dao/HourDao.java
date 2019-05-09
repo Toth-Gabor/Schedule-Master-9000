7b5f48f2342de90f48c21fbf647eacd4155bacd1
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-09 13:00:21 +0200

    added find day id by hour value method, removed task id

--- src/main/java/com/codecool/web/dao/HourDao.java
+++ src/main/java/com/codecool/web/dao/HourDao.java
@@ -9,7 +9,8 @@ public interface HourDao {
     
     List<Hour> findAll() throws SQLException;
     List<Hour> findbyHourValue(int hourValue) throws SQLException;
-    List<Hour> findbyDayId(int taskId) throws SQLException;
+    List<Hour> findbyDayId(int dayId) throws SQLException;
+    int findDayIdbyHourValue(int hourValue, int dayId) throws SQLException;
     void delete(Hour hour) throws SQLException;
     void add(int hourValue, int dayId) throws SQLException;
     void update(Hour hour, int hourValue) throws SQLException;
