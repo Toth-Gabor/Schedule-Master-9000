c20b09a969ba4927adfcf9c1ec09c11824d94f7d
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-15 13:26:14 +0200

    removed scheduleS content so you can only see it if you click under Links header1

--- src/main/webapp/back-to-profile.js
+++ src/main/webapp/back-to-profile.js
@@ -1,3 +1,3 @@
 function onBackToProfileClicked() {
-    showContents(['profile-content', 'logout-content']);
+    showContents(['schedule-content', 'profile-content', 'logout-content', 'add-schedule-content']);
 }
