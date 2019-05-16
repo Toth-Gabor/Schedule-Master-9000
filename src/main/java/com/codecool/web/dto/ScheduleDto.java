108cc78a16f76159b0d8b70e50fa366d8da40eb2
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-16 15:18:10 +0200

    sendmessage now sends alltasknameslist to js

--- src/main/java/com/codecool/web/dto/ScheduleDto.java
+++ src/main/java/com/codecool/web/dto/ScheduleDto.java
@@ -13,27 +13,33 @@ public class ScheduleDto {
     private final List<Day> dayList;
     private final List<Task> taskList;
     private final List<Hour> hourList;
-    
-    public ScheduleDto(Schedule schedule, List<Day> dayList, List<Task> taskList, List<Hour> hourList) {
+    private final String[][] allTaskNames;
+
+    public ScheduleDto(Schedule schedule, List<Day> dayList, List<Task> taskList, List<Hour> hourList, String[][] allTaskNames) {
         this.schedule = schedule;
         this.dayList = dayList;
         this.taskList = taskList;
         this.hourList = hourList;
+        this.allTaskNames = allTaskNames;
     }
-    
+
     public Schedule getSchedule() {
         return schedule;
     }
-    
+
     public List<Day> getDayList() {
         return dayList;
     }
-    
+
     public List<Task> getTaskList() {
         return taskList;
     }
-    
+
     public List<Hour> getHourList() {
         return hourList;
+    }
+
+    public String[][] getAllTaskNames() {
+        return allTaskNames;
     }
 }
