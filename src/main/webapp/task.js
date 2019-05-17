f0f69dffc0a2c25beb4d5c0b74d84c3a4805ed5f
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-17 15:41:18 +0200

    add task method created

--- src/main/webapp/task.js
+++ src/main/webapp/task.js
@@ -2,7 +2,6 @@ function onTaskResponse() {
     if (this.status === OK) {
         clearMessages();
         showContents(['link-content', 'tasks-content', 'task-fields','back-to-profile-content']);
-        console.log(JSON.parse(this.responseText));
         onTaskLoad(JSON.parse(this.responseText));
     } else {
         onOtherResponse(tasksContentDivEl, this);
@@ -15,5 +14,19 @@ function onTaskLoad(taskList) {
 
     taskNameSpanEl.innerHTML = taskList.name;
     taskContentSpanEl.innerHTML = taskList.content;
+
+}
+function onAddTaskClicked() {
+    const ScheduleId = this.dataset.ScheduleId;
+
+    //kell valahogy a schedule id a kattintásból
+    //kell egy új dao a hour_task insertre
+
+
+
+    //1. kiválasztod melyik taskot
+    //2. kiválasztod melyik napra --> schedule id alapján napok listája
+    //3. kiválasztod hény órára -- > naplista alapján óra lista
+    //add és deletet egymás után meghívni vagy trigger
 
 }
