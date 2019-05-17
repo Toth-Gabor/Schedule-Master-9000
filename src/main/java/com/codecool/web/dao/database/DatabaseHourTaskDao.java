e5186047fa9f2c5d02316498dd1c625459587210
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-17 15:40:08 +0200

    basic delete

--- src/main/java/com/codecool/web/dao/database/DatabaseHourTaskDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseHourTaskDao.java
@@ -1,4 +1,23 @@
 package com.codecool.web.dao.database;
 
-public class DatabaseHourTaskDao {
+import com.codecool.web.dao.HourTaskDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseHourTaskDao extends AbstractDao implements HourTaskDao {
+
+    DatabaseHourTaskDao(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public void delete(int taskId) throws SQLException {
+        String sql = "DELETE FROM hour_task WHERE task_id = ?;";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, taskId);
+            statement.execute();
+        }
+    }
 }
