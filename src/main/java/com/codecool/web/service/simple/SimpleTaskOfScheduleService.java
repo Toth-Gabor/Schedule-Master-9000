71b67afb42ac11809802fc23121e24be9237aa23
Toth-Gabor <upcgabor@gmail.com>
2019-05-21 13:50:02 +0200

    TaskOfSchedule Services implemented

---
+++ src/main/java/com/codecool/web/service/simple/SimpleTaskOfScheduleService.java
@@ -0,0 +1,20 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.TaskOfScheduleDao;
+import com.codecool.web.service.TaskOfScheduleService;
+
+import java.sql.SQLException;
+
+public class SimpleTaskOfScheduleService implements TaskOfScheduleService {
+
+    private final TaskOfScheduleDao taskOfScheduleDao;
+    
+    public SimpleTaskOfScheduleService(TaskOfScheduleDao taskOfScheduleDao) {
+        this.taskOfScheduleDao = taskOfScheduleDao;
+    }
+    
+    @Override
+    public void addHourIdAndTask(int hourId, int taskId) throws SQLException {
+        taskOfScheduleDao.add(hourId, taskId);
+    }
+}
