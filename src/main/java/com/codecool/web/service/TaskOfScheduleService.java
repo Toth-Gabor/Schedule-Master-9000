53382797015d9f3b4fb648f26df836bfba96ea2c
Toth-Gabor <upcgabor@gmail.com>
2019-05-22 08:29:54 +0200

    taskOfSchedule Service added to git

---
+++ src/main/java/com/codecool/web/service/TaskOfScheduleService.java
@@ -0,0 +1,8 @@
+package com.codecool.web.service;
+
+import java.sql.SQLException;
+
+public interface TaskOfScheduleService {
+    
+    void addHourIdAndTask(int hourId, int taskId) throws SQLException;
+}
