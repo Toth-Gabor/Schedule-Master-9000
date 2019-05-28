fa6f9dbc614b5932f5f981751d8cdf1bf0c75e4e
Toth-Gabor <upcgabor@gmail.com>
2019-05-28 11:17:49 +0200

    userService implemented

---
+++ src/main/java/com/codecool/web/service/simple/SimpleUserService.java
@@ -0,0 +1,21 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+
+import java.sql.SQLException;
+
+public class SimpleUserService implements UserService {
+    
+    private final UserDao userDao;
+    
+    public SimpleUserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    
+    @Override
+    public void addUser(String name, String password, String email, boolean isAdmin) throws SQLException, SecurityException{
+        userDao.add(name, email, password, isAdmin);
+    }
+}
