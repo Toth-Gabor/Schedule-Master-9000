24966fd2d0d02b1efeb059bdcbef1abb0f2595da
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-28 16:36:02 +0200

    imlemented check method in service

--- src/main/java/com/codecool/web/service/simple/SimpleTaskOfScheduleService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleTaskOfScheduleService.java
@@ -2,6 +2,7 @@ package com.codecool.web.service.simple;
 
 import com.codecool.web.dao.TaskOfScheduleDao;
 import com.codecool.web.service.TaskOfScheduleService;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 
@@ -14,12 +15,18 @@ public class SimpleTaskOfScheduleService implements TaskOfScheduleService {
     }
     
     @Override
-    public void addHourIdAndTask(int hourId, int taskId) throws SQLException {
-        taskOfScheduleDao.add(hourId, taskId);
+    public void addHourIdAndTask(int hourId, int taskId, int scheduleId) throws SQLException, ServiceException {
+        if (taskOfScheduleDao.checkIfExsists(scheduleId ,taskId)){
+            throw new ServiceException("Task already exist in current schedule");
+        } else {
+            taskOfScheduleDao.add(hourId, taskId);
+        }
     }
     
     @Override
     public void deleteTaskFromScheduleById(int hourId) throws SQLException {
         taskOfScheduleDao.delete(hourId);
     }
+
+
 }
