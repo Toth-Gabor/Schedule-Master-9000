82311c119b7e19a0f11c957fd8641da0e7dbc82b
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-03 14:57:56 +0200

    added validate email method for service

--- src/main/java/com/codecool/web/service/UserService.java
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -1,10 +1,13 @@
 package com.codecool.web.service;
 
+import com.codecool.web.model.User;
 import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 
 public interface UserService {
     
-    void addUser(String name, String password, String email, boolean isAdmin) throws SQLException, ServiceException;
+    void addUser(String name, String email, String password, boolean isAdmin) throws SQLException, ServiceException;
+    boolean validateEmail(String email) throws SQLException, ServiceException;
+    User getbyEmail(String email) throws ServiceException, SQLException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleUserService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleUserService.java
@@ -3,6 +3,7 @@ package com.codecool.web.service.simple;
 import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
+import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
 
@@ -17,5 +18,15 @@ public class SimpleUserService implements UserService {
     @Override
     public void addUser(String name, String email, String password, boolean isAdmin) throws SQLException, SecurityException{
         userDao.add(name, email, password, isAdmin);
+    }
+
+    @Override
+    public boolean validateEmail(String email) throws SQLException, ServiceException {
+        return userDao.validateEmail(email);
+    }
+
+    @Override
+    public User getbyEmail(String email) throws ServiceException, SQLException {
+        return userDao.findByEmail(email);
     }
 }
