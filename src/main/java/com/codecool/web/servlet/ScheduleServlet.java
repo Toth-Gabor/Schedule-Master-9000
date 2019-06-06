afff9fbe7033961531a27435724d1b2e8f7d8a79
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-06 10:05:23 +0200

    removed logic from servlet, implemented it in service

--- src/main/java/com/codecool/web/servlet/ScheduleServlet.java
+++ src/main/java/com/codecool/web/servlet/ScheduleServlet.java
@@ -33,64 +33,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/protected/schedule")
-public class ScheduleServlet extends AbstractServlet{
-    
+public class ScheduleServlet extends AbstractServlet {
+
     private static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
     private static final Logger logger = LoggerFactory.getLogger(ScheduleServlet.class);
 
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (Connection connection = getConnection(req.getServletContext())){
+        try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
             ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
             String schId = req.getParameter("schedule-id");
             int scheduleId = Integer.parseInt(schId);
             Schedule schedule = scheduleService.getbyId(scheduleId);
-            
+
             DayDao dayDao = new DatabaseDayDao(connection);
             DayService dayService = new SimpleDayService(dayDao);
             List<Day> dayList = dayService.getByScheduleId(scheduleId);
-            
+
             TaskDao taskDao = new DatabaseTaskDao(connection);
             TaskService taskService = new SimpleTaskService(taskDao);
             List<Task> taskList = taskService.getbyScheduleId(scheduleId);
-    
+
             HourDao hourDao = new DatabaseHourDao(connection);
             HourService hourService = new SimpleHourService(hourDao);
-            List<Integer> dayIdList = new ArrayList<>();
-            String[][] allTaskNames = new String[dayList.size()][24];
 
 
-            //service
-            for (int i = 0; i < dayList.size() ; i++) {
-                dayIdList.add(dayList.get(i).getId());
-                String[] tasknames = taskDao.findhourContentList(dayList.get(i).getId());
-                allTaskNames[i] = tasknames;
-
-            }
+            String[][] allTaskNames = scheduleService.getAllTaskNames(dayList, connection);
 
             List<Hour> hourList = new ArrayList<>();
-            for (int i = 0; i < dayIdList.size(); i++) {
-                List<Hour> hourListForDayId = hourService.getbyDayId(dayIdList.get(i));
+            for (int i = 0; i < dayList.size(); i++) {
+                List<Hour> hourListForDayId = hourService.getbyDayId(dayList.get(i).getId());
                 for (Hour hour : hourListForDayId) {
                     hourList.add(hour);
                 }
             }
 
-            
+
             sendMessage(resp, HttpServletResponse.SC_OK, new ScheduleDto(schedule, dayList, taskList, hourList, allTaskNames));
-        
+
         } catch (SQLException e) {
             handleSqlError(resp, e);
         } catch (ServiceException e) {
             e.printStackTrace();
         }
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try (Connection connection = getConnection(req.getServletContext())){
+        try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
             ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
             User user = (User) req.getSession().getAttribute("user");
@@ -111,13 +103,13 @@ public class ScheduleServlet extends AbstractServlet{
             logger.error("exception on adding schedule", e);
         }
     }
-    
+
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
             ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
-            int scheduleId = Integer.parseInt( req.getParameter("schedule-id"));
+            int scheduleId = Integer.parseInt(req.getParameter("schedule-id"));
             boolean isPublished = Boolean.parseBoolean(req.getParameter("schedule-published"));
 
             Schedule schedule = scheduleService.getbyId(scheduleId);
@@ -138,7 +130,7 @@ public class ScheduleServlet extends AbstractServlet{
             String[][] allTaskNames = new String[dayList.size()][24];
 
 
-            for (int i = 0; i < dayList.size() ; i++) {
+            for (int i = 0; i < dayList.size(); i++) {
                 dayIdList.add(dayList.get(i).getId());
                 String[] tasknames = taskDao.findhourContentList(dayList.get(i).getId());
                 allTaskNames[i] = tasknames;
@@ -156,7 +148,7 @@ public class ScheduleServlet extends AbstractServlet{
 
             sendMessage(resp, HttpServletResponse.SC_OK, new ScheduleDto(schedule, dayList, taskList, hourList, allTaskNames));
             logger.info("schedule updated");
-            
+
         } catch (SQLException e) {
             handleSqlError(resp, e);
             logger.error("sql error on updating", e);
@@ -165,17 +157,17 @@ public class ScheduleServlet extends AbstractServlet{
             logger.error("error on updating schedule");
         }
     }
-    
+
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (Connection connection = getConnection(req.getServletContext())){
+        try (Connection connection = getConnection(req.getServletContext())) {
             ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
             ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
             int scheduleId = Integer.parseInt(req.getParameter("schedule-id"));
             Schedule schedule = scheduleService.getbyId(scheduleId);
             scheduleService.delete(schedule);
             logger.info("schedule deleted");
-            
+
         } catch (SQLException e) {
             handleSqlError(resp, e);
             logger.error("sql exception on deleting schedule", e);
